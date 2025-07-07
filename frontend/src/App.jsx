e27bcfcf9bdfa16537e61b7fdfd915589ffef351
gzmcn <220601117@bakircay.edu.tr>
2025-07-07 11:33:15 +0300

    note+frontend start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDom from "react-dom/client"
+import App from 'App.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
